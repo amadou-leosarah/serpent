65b76440d189519b4b3b15676e7860c56f5d014a
Eliot eliot <leosarah109@gmail.com>
2021-01-09 13:52:59 +0300

    Created Constructors and encapsulation

--- Serpent/Program.cs
+++ Serpent/Program.cs
@@ -10,7 +10,12 @@ namespace Serpent
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
             Console.ReadLine();
         }
     }
