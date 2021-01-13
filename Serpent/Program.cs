64b06e02611342668dca009b5a45a7b4e222bd49
Eliot eliot <leosarah109@gmail.com>
2021-01-13 21:44:34 +0300

    Game Over

--- Serpent/Program.cs
+++ Serpent/Program.cs
@@ -2,16 +2,76 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Serpent
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
-            Console.ReadLine();
-        }
-    }
+		static void Main(string[] args)
+		{
+			Console.SetBufferSize(127, 39);
+
+			Walls walls = new Walls(127, 39);
+			walls.Draw();
+
+			// Отрисовка точек			
+			Point p = new Point(4, 5, '*');
+			Snake snake = new Snake(p, 4, Direction.RIGHT);
+			snake.Draw();
+
+			FoodCreator foodCreator = new FoodCreator(127, 39, '$');
+			Point food = foodCreator.CreateFood();
+			food.Draw();
+
+			while (true)
+			{
+				if (walls.IsHit(snake) || snake.IsHitTail())
+				{
+					break;
+				}
+				if (snake.Eat(food))
+				{
+					food = foodCreator.CreateFood();
+					food.Draw();
+				}
+				else
+				{
+					snake.Move();
+				}
+
+				Thread.Sleep(100);
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey(key.Key);
+				}
+			}
+			WriteGameOver();
+			Console.ReadLine();
+		}
+
+
+		static void WriteGameOver()
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.SetCursorPosition(xOffset, yOffset++);
+			WriteText("=================", xOffset, yOffset++);
+			WriteText("  GAME OVER!!", xOffset + 1, yOffset++);
+			yOffset++;
+			WriteText("Author: ELIOT", xOffset + 2, yOffset++);
+			WriteText("FROM GEEKBRAINS", xOffset + 1, yOffset++);
+			WriteText("=================", xOffset, yOffset++);
+		}
+
+		static void WriteText(String text, int xOffset, int yOffset)
+		{
+			Console.SetCursorPosition(xOffset, yOffset);
+			Console.WriteLine(text);
+		}
+
+	}
 }
