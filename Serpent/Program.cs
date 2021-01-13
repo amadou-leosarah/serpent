3926f3096e450540a8102dcc10deda5ffd9567b0
Eliot eliot <leosarah109@gmail.com>
2021-01-13 21:31:05 +0300

    Hit the Walls

--- Serpent/Program.cs
+++ Serpent/Program.cs
@@ -2,32 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Serpent
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // Window size
-            Console.SetBufferSize(150, 95);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.SetBufferSize(127, 39);
 
-            // Drawing a fram
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-            upLine.Drow();
-            downLine.Drow();
-            leftLine.Drow();
-            rightLine.Drow();
+			Walls walls = new Walls(127, 39);
+			walls.Draw();
 
-            // Drawing points
-            Point p = new Point(4, 5, '*');
-            p.Draw();
+			// Отрисовка точек			
+			Point p = new Point(4, 5, '*');
+			Snake snake = new Snake(p, 4, Direction.RIGHT);
+			snake.Draw();
 
-            Console.ReadLine();
-        }
-    }
+			FoodCreator foodCreator = new FoodCreator(127, 39, '$');
+			Point food = foodCreator.CreateFood();
+			food.Draw();
+
+			while (true)
+			{
+				if (walls.IsHit(snake) || snake.IsHitTail())
+				{
+					break;
+				}
+				if (snake.Eat(food))
+				{
+					food = foodCreator.CreateFood();
+					food.Draw();
+				}
+				else
+				{
+					snake.Move();
+				}
+
+				Thread.Sleep(100);
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey(key.Key);
+				}
+			}
+		}
+
+	}
 }
